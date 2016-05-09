e1201e5d93003440eb99e81baaba11ffd1df1c2e
G Faulkner <glyn.faulkner@york.ac.uk>
2016-05-09 08:14:36 +0100

    Implemented a rudimentary automated test suite for gp2c

--- Haskell/Tests/run-tests.sh
+++ Haskell/Tests/run-tests.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 GP2C_OPTS=""
-GP2C="../gp2c $GP2C_OPTS"
-
-TESTS="bidi bidi2 colour looped-rule-set negatives rooted-2-colouring root rule-set transitive_closure"
-
+GP2C="`pwd`/../gp2c $GP2C_OPTS"
+ISO="`pwd`/../IsoChecker"
 
 
-function clean_test_dir () {
-	
+failure () {
+	echo -e "\t\e[31mfailure\e[0m"
 }
 
+success () {
+	echo -e "\t\e[success\e[0m"
+}
 
-for t in $TESTS ; do
-	$GP2C $t.gp2 $t.host 
+for d in */ ; do
+	p=${d%%/}
+	pushd $d > /dev/null
+	for t in *.host ; do
+		echo -n Testing $t in $d
+		( $GP2C $p.gp2 $t && ./$p > /tmp/$p.out && $ISO /tmp/$p.out Solutions/$t ) > /dev/null 2>&1 && success || failure
+	done
+	rm -f $p $p.c
+	popd > /dev/null
 done
 
